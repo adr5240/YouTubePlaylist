4b1dda548a2fd0848da8e21b4cc3cf3f5336c75e
Adam Reiter <adr5240@gmail.com>
2017-04-03 12:21:44 -0400

    Connect views tmpls to index.html

--- app/list/videoList.js
+++ app/list/videoList.js
@@ -1,3 +1,32 @@
 angular.module('videos', [
     'youtube.models.videos'
-]);
+])
+    .config(function($stateProvider) {
+        $stateProvider
+            .state('youtube.videos', {
+                url: '/',
+                views: {
+                    'videos-tablet@': {
+                        controller: 'VideosListCtrl as videosListCtrl',
+                        templateUrl: 'app/list/videoList-tablet.tmpl.html'
+                    },
+                    'videos-mobile@': {
+                        controller: 'VideosListCtrl as videosListCtrl',
+                        templateUrl: 'app/list/videoList-mobile.tmpl.html'
+                    },
+                    'details-tablet@': {
+                        controller: 'DetailsCtrl as detailsCtrl',
+                        templateUrl: 'app/list/detail/videoDetail-tablet.tmpl.html'
+                    },
+                    'details-mobile@': {
+                        controller: 'DetailsCtrl as detailsCtrl',
+                        templateUrl: 'app/list/detail/videoDetail-mobile.tmpl.html'
+                    }
+                }
+            });
+
+    })
+    .controller('VideosListCtrl', function VideosCtrl(VideosModel) {
+
+    })
+;
